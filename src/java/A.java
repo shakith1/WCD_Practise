9124eefcc1cd0e36d12fa02fe0a4ef8fb3f07912
Shakith <nimsara.shakith@gmail.com>
2022-10-08 14:58:21 +0530

    Calling Super method

--- src/java/A.java
+++ src/java/A.java
@@ -21,6 +21,7 @@ public class A extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.getWriter().write("HEllo");
+        super.doGet(req, resp);
     }
     
 }
